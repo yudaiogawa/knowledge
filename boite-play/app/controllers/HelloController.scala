5653e4dc36342e1eda459b1cfec353f8de0791e1
yudaiogawa <19630931+yudaiogawa@users.noreply.github.com>
2021-02-02 03:26:59 +0900

    update: use i18n

--- boite-play/app/controllers/HelloController.scala
+++ boite-play/app/controllers/HelloController.scala
@@ -1,21 +1,17 @@
 package controllers
 
 import javax.inject.Inject
-import javax.inject.Singleton
-import play.api.mvc.AbstractController
-import play.api.mvc.AnyContent
-import play.api.mvc.ControllerComponents
-import play.api.mvc.Request
+import play.api.i18n.{I18nSupport, Messages, MessagesApi}
+import play.api.mvc.{Action, AnyContent, Controller, Request}
 
-@Singleton
-class HelloController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HelloController @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
   def get(name: Option[String]) =
     Action { implicit request: Request[AnyContent] =>
       Ok {
         name
-          .map(s => s"Hello, $s :)")
-          .getOrElse("Please give a name")
+          .map(s => Messages("hello", s))
+          .getOrElse(Messages("noQuery"))
       }
     }
 }
